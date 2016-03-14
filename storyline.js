35ebf64e69cada47ca9520e1fbbe7e18ed8713b0
Jordan Delcros <contact@jordan-delcros.com>
2016-03-14 13:16:31 +0100

    add types to filter values output (like vec3 or int)

--- storyline.js
+++ storyline.js
@@ -4,35 +4,40 @@
 		TIME: 0,
 		EASING: 1,
 		OPTION: 2,
-		VALUE: 3
+		VALUE: 3,
+		TYPE: 4
 	};
 
+	var types = new Array();
 	var easings = new Array();
 
-	var Storyline = function( story, duration ){
+	var Storyline = function( story, timeScale ){
 
-		return new Storyline.methods.initialize(story, duration);
+		return new Storyline.methods.initialize(story, timeScale);
 
 	};
 
 	Storyline.methods = Storyline.prototype = {
 		constructor: Storyline,
-		initialize: function( story, duration ){
+		initialize: function( story, timeScale ){
 
 			this.storyboard = new Object();
 
-			this.duration = (parseFloat(duration) || 1);
+			this.timeScale = (parseFloat(timeScale) || 1);
+
+			this.duration = 0;
 
 			for( var key in story ){
 
 				for( var step in story[key] ){
-					
+
 					var time = parseFloat(story[key][step].match(/^\s*([0-9]+\.?[0-9]*)/g)[0]);
 
-					var easing = story[key][step].match(/([a-z]+)(?:\((.*)\))?/);
+					var easing = story[key][step].match(/([a-z]+)(?:\((.*)\))?/i);
 					var easingMode = Storyline.getEasing(easing[1]);
 					var easingOptions = (easing[2] != undefined ? easing[2].split(/\,/g) : null);
 
+					var type = Storyline.getType((story[key][step].match(/([a-z0-9]+)\([^\)]+\)$/) || "")[1]);
 					var extractedValue = story[key][step].match(/(?:(\-?\s*[0-9]+\.?[0-9]*)|\(([^\)]+)\))$/g)[0].match(/(\-?\s*[0-9]+\.?[0-9]*)/g);
 					var value = extractedValue.map(function( number ){
 
@@ -40,7 +45,7 @@
 
 					});
 
-					this.set(key, time, easingMode, easingOptions, value);
+					this.set(key, time, easingMode, easingOptions, value, type);
 
 				};
 
@@ -49,7 +54,11 @@
 			return this;
 
 		},
-		set: function( key, time, easing, options, value ){
+		set: function( key, time, easing, options, value, type ){
+
+			time *= this.timeScale;
+
+			this.duration = Math.max(this.duration, time);
 
 			if( this.storyboard[key] == undefined ){
 
@@ -57,7 +66,7 @@
 
 			};
 
-			this.storyboard[key].push([(time * this.duration), easing, options, value]);
+			this.storyboard[key].push([time, easing, options, value, type]);
 
 			this.storyboard[key].sort(function( before, after ){
 
@@ -72,38 +81,49 @@
 
 			if( this.storyboard[key] != undefined ){
 
+				var type = null;
+				var values = new Array();
+
 				for( var stepMax = this.storyboard[key].length - 1, step = stepMax; step >= 0; step-- ){
+
+					type = (types[this.storyboard[key][step][KEY.TYPE]] || null);
 
 					if( this.storyboard[key][step][KEY.TIME] <= now ){
 
 						var from = this.storyboard[key][step];
 						var to = this.storyboard[key][Math.min(step + 1, stepMax)];
 
-						var values = new Array();
-
 						for( var valueIndex = 0, length = to[KEY.VALUE].length; valueIndex < length; valueIndex++ ){
 
 							var difference = to[KEY.VALUE][valueIndex] - from[KEY.VALUE][valueIndex];
 							var duration = to[KEY.TIME] - from[KEY.TIME];
 
-							var elapsed = Math.min((((now - from[KEY.TIME]) / duration) || 0), 1)
+							var elapsed = Math.min((((now - from[KEY.TIME]) / duration) || 0), 1);
 
-							values[valueIndex] = from[KEY.VALUE][valueIndex] + (easings[to[KEY.EASING]][KEY.EASING](elapsed, 1, to[KEY.OPTION]) * difference);
+							values[valueIndex] = from[KEY.VALUE][valueIndex] + (easings[to[KEY.EASING]][1](elapsed, 1, to[KEY.OPTION]) * difference);
 
 						};
 
-						return (values.length == 1 ? values[0] : values);
+						break;
 
 					}
 					else if( step == 0 ){
 
-						var values = this.storyboard[key][step][KEY.VALUE];
+						values = this.storyboard[key][step][KEY.VALUE];
 
-						return (values.length == 1 ? values[0] : values);
+						break;
 
 					};
 
 				};
+
+				if( type != null ){
+
+					values = type[1](values);
+
+				};
+
+				return (values.length == 1 ? values[0] : values);
 
 			};
 
@@ -114,15 +134,66 @@
 
 	Storyline.methods.initialize.prototype = Storyline.methods;
 
+	Storyline.registerType = function( name, typeFunction ){
+
+		types.push([name, typeFunction]);
+
+	};
+
+	Storyline.getType = function( name ){
+
+		for( var type = 0, length = types.length; type < length; type++ ){
+
+			if( name == types[type][0] ){
+
+				return type;
+
+			};
+
+		};
+
+		return null;
+
+	};
+
+	Storyline.registerType("int", function( options ){
+
+		for( var option = 0, length = options.length; option < length; option++ ){
+
+			options[option] = parseInt(options[option]);
+
+		};
+
+		return options;
+
+	});
+
+	Storyline.registerType("vec2", function( options ){
+
+		options.x = options[0];
+		options.y = options[1];
+
+		return options;
+
+	});
+
+	Storyline.registerType("vec3", function( options ){
+
+		options.x = options[0];
+		options.y = options[1];
+		options.z = options[2];
+
+		return options;
+
+	});
+
 	Storyline.registerEasing = function( name, easingFunction ){
 
-		easings.push([name.toLowerCase(), easingFunction]);
+		easings.push([name, easingFunction]);
 
 	};
 
 	Storyline.getEasing = function( name ){
-
-		name = name.toLowerCase();
 
 		for( var easing = 0, length = easings.length; easing < length; easing++ ){
 
@@ -150,19 +221,19 @@
 
 	});
 
-	Storyline.registerEasing("easein", function( elapsed, duration, options ){
+	Storyline.registerEasing("easeIn", function( elapsed, duration, options ){
 
 		return elapsed * elapsed * elapsed;
 
 	});
 
-	Storyline.registerEasing("easeout", function( elapsed, duration, options ){
+	Storyline.registerEasing("easeOut", function( elapsed, duration, options ){
 
 		return (elapsed -= 1) * elapsed * elapsed + 1;
 
 	});
 
-	Storyline.registerEasing("easeinout", function( elapsed, duration, options ){
+	Storyline.registerEasing("easeInOut", function( elapsed, duration, options ){
 
 		if( (elapsed /= duration / 2) < 1 ){
 
